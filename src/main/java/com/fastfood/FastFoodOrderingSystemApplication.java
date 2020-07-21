8448048228862a6542aec26e354224be910325cc
JovanPapi <jovan.gppapi@hotmail.com>
2020-07-21 16:17:48 +0200

    Create new project

---
+++ src/main/java/com/fastfood/FastFoodOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fastfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodOrderingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastFoodOrderingSystemApplication.class, args);
+    }
+
+}
