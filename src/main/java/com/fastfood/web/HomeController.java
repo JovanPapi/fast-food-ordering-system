8a082ceccfc203f3a11249927d1acb0890e59d56
JovanPapi <jovan.gppapi@hotmail.com>
2020-08-12 20:46:56 +0200

    View changes

--- src/main/java/com/fastfood/web/HomeController.java
+++ src/main/java/com/fastfood/web/HomeController.java
@@ -1,6 +1,7 @@
 package com.fastfood.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class HomeController {
 
     private String homePage(){
         return "index";
+    }
+
+    @GetMapping("/contact-us")
+    private String contactPage(){
+        return "contact";
     }
 }
