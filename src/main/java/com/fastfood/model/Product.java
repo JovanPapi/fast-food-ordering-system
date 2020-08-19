fae4f2cef6d9a6e96bfa3908906d117fdc00fa8c
Kristijan <kristijan_98@live.com>
2020-08-19 13:00:36 +0200

    chicken

--- src/main/java/com/fastfood/model/Product.java
+++ src/main/java/com/fastfood/model/Product.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "products")
@@ -34,5 +35,6 @@ public class Product {
 
     @ManyToMany(mappedBy = "order")
     private List<User> userOrders;
+
 
 }
